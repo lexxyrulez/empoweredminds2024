20a73a2d2aa7c660c9ded3e01ac55f1a28d5fa55
lexxyrulez <alexchambika85@gmail.com>
2025-04-16 09:25:49 +0300

    CMS

--- resources/views/emails/contact_form.blade.php
+++
@@ -1,7 +0,0 @@
-<h2>New Client</h2>
-<p><strong>Name:</strong> {{ $data['name'] }}</p>
-<p><strong>Phone:</strong> {{ $data['phone'] }}</p>
-<p><strong>Email:</strong> {{ $data['email'] }}</p>
-<p><strong>Subject:</strong> {{ $data['subject'] }}</p>
-<p><strong>Message:</strong></p>
-<p>{{ $data['message'] }}</p>
